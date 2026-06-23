51c5a5a72eee5755bd91be08fb181a65247d61e7
kokissik63-cmd <kokissik63@gmail.com>
2026-06-23 21:47:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-import os
-import asyncio
-from flask import Flask, request
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TOKEN = "8825832204:AAHbHrkpybRJBu_HBiotjevPB58J6ZaJ_pA"
 GROUP_ID = -1003904422279
 
-app = Flask(__name__)
-
-application = Application.builder().token(TOKEN).build()
-
 msg_to_user = {}
 
-# ======================
-# ЛОГИКА БОТА
-# ======================
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message
     if not msg:
@@ -36,9 +26,11 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif msg.chat.type in ["group", "supergroup"]:
 
         if msg.reply_to_message:
+
             replied_id = msg.reply_to_message.message_id
 
             if replied_id in msg_to_user:
+
                 user_id = msg_to_user[replied_id]
 
                 await context.bot.send_message(
@@ -46,29 +38,7 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     text=f"💬 Ответ:\n\n{msg.text}"
                 )
 
-application.add_handler(MessageHandler(filters.ALL, handle))
+app = Application.builder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, handle))
 
-# ======================
-# WEB SERVER (Render)
-# ======================
-@app.route("/")
-def home():
-    return "Bot is running"
-
-@app.route(f"/{TOKEN}", methods=["POST"])
-def webhook():
-    update = Update.de_json(request.get_json(force=True), application.bot)
-    application.update_queue.put_nowait(update)
-    return "ok"
-
-# ======================
-# START
-# ======================
-if __name__ == "__main__":
-    async def run():
-        await application.initialize()
-        await application.start()
-
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
-    asyncio.run(run())
+app.run_polling()
